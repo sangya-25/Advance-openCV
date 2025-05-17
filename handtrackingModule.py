530add72a6b55f87ca3fc97267d58e396cc18229
sangya-25 <ojha.sangya25@gmail.com>
2025-05-17 11:45:39 +0530

    Done with project-5 (last project of the Advance openCV tutorial ) created a virtual mouse cursor which can click and can move using hand tracking module

--- handtrackingModule.py
+++ handtrackingModule.py
@@ -1,7 +1,7 @@
 import cv2 
 import mediapipe as mp
 import time
-
+import math
 
 #initializing everything under this class which are requisite
 class handDetector():
@@ -36,27 +36,41 @@ class handDetector():
                 
 
     def findPosition(self, img, handNo=0, draw=True):
-        self.lmList=[]
+        bbox = []
+        self.lmList = []
         if self.results.multi_hand_landmarks:
-            myHand=self.results.multi_hand_landmarks[handNo] #it will detect the first hand 
-            for id, lm in enumerate(myHand.landmark): #it will return the id for each and evry landmark in a hand
-                    #print(id,lm) 
-                    h,w,c=img.shape
-                    cx,cy=int(lm.x*w), int(lm.y*h)  # to get the value in integer instead of floating values
-                    #multiplied x value of landmark with width and y with height to get the exact central location 
-                    self.lmList.append([id, cx,cy])
-                    #to highlight a particular landmark
-                    if draw: 
-                        cv2.circle(img,(cx,cy),5,(0,0,255),cv2.FILLED)
+            myHand = self.results.multi_hand_landmarks[handNo]
+            xList = []
+            yList = []
+            for id, lm in enumerate(myHand.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                xList.append(cx)
+                yList.append(cy)
+                # print(id, cx, cy)
+                self.lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+            if xList and yList:
+                xmin, xmax = min(xList), max(xList)
+                ymin, ymax = min(yList), max(yList)
+                bbox = [xmin, ymin, xmax, ymax]
 
-        return self.lmList
+                if draw:
+                    cv2.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20),
+                    (0, 255, 0), 2)
+
+        return self.lmList, bbox
     
     def fingersUp(self):
        
         fingers=[]
+        if len(self.lmList) == 0:
+            return fingers 
         #for thumb : not checking on the basis of y-parameter rather checking if point 4 goes beyond the point 3 then considering to be open
         #else considering top be closed(on the right of point 3 of thumb)
-        if self.lmList[4][1] > self.lmList[4-1][1]:  
+        if self.lmList[4][1] < self.lmList[4-1][1]:  
             fingers.append(0)
             # print("Index finger open")    
         else:
@@ -70,6 +84,20 @@ class handDetector():
             else:
                 fingers.append(0) 
         return fingers
+    
+    def findDistance(self, p1, p2, img, draw=True,r=15, t=3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+            length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
 
 def main():
     cap=cv2.VideoCapture(0)
@@ -79,7 +107,7 @@ def main():
     while True:
         success, img=cap.read()
         img=detector.findHands(img)
-        lmList=detector.findPosition(img)
+        lmList,bbox=detector.findPosition(img)
         if len(lmList)!=0:
             print(lmList[4])
         cTime=time.time()
